eeb59609c5c5a149c05cc1159045f2507b00e27f
Michale-Cortez <CortezDevelopement@gmail.com>
2020-04-08 18:56:25 -0600

    Waiting for auth ready to git rid of flicker with showing unwanted links on refresh. found at docs here => http://react-redux-firebase.com/docs/recipes/auth.html#wait-for-auth-to-load

--- src/index.js
+++ src/index.js
@@ -1,40 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './store/reducers/rootReducer'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore'
-import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
-import fbConfig from './config/fbConfig'
-import firebase from 'firebase/app'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import {
+  createFirestoreInstance,
+  getFirestore,
+  reduxFirestore
+} from "redux-firestore";
+import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
+import firebase from "firebase/app";
+
+//for render on auth ready
+import { useSelector  } from 'react-redux'
+import { isLoaded  } from 'react-redux-firebase';
+
+
 
 const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
-        reduxFirestore(firebase, fbConfig)
-    )
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+    reduxFirestore(firebase, fbConfig)
+  )
 );
+
+const profileSpecificProps = {
+  userProfile: 'users',
+  useFirestoreForProfile: true,
+  enableRedirectHandling: false,
+  resetBeforeLogin: false
+}
+
 
 const rrfProps = {
-    firebase,
-    config: fbConfig,
-    dispatch: store.dispatch,
-    createFirestoreInstance
+  firebase,
+  config: fbConfig,
+  config: profileSpecificProps,
+  dispatch: store.dispatch,
+  createFirestoreInstance
 };
 
+function AuthIsLoaded({ children }) {
+  const auth = useSelector(state => state.firebase.auth)
+  if (!isLoaded(auth)) {
+    return (
+      <div className="auth-ready-container">
+        <p>Loading...</p>
+      </div>
+    );
+  } else {
+    return children
+  }
+}
+
+
 ReactDOM.render(
-    <Provider store={store}>
-        <ReactReduxFirebaseProvider {...rrfProps}>
-            <App />
-        </ReactReduxFirebaseProvider>
-    </Provider>,
-    document.getElementById("root")
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <AuthIsLoaded>
+      <App />
+      </AuthIsLoaded>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
-
-serviceWorker.unregister(); 
+serviceWorker.unregister();
